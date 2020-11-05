1c6ec29cacf535c1a3883dc95d7679844c12bdd8
Umang Raj <umangraj@Umangs-MacBook-Air.local>
2020-11-05 12:10:04 +0530

    Adding port

--- server.js
+++ server.js
@@ -66,6 +66,6 @@ app.post('/imageurl' ,(req,res)=>{
   image.handleApiCall(req,res);
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('app is running');
 })
