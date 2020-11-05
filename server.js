856fff6f4e5e97ba17de675cd0f5fb57dc3c13ef
Umang Raj <umangraj@Umangs-MacBook-Air.local>
2020-11-05 12:03:49 +0530

    Pushing Server to Git

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex=require('knex');
+const register=require('./controllers/register');
+const signin=require('./controllers/signin');
+const profile=require('./controllers/profile');
+const image=require('./controllers/image');
+
+const db =knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'umangraj',
+    password : '',
+    database : 'face-app'
+  }
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const database = {
+	users: [
+    {
+    	id: '123',
+    	name: 'John',
+    	email: 'john@gmail.com',
+    	password: 'cookies',
+    	entries: 0,
+    	joined: new Date()
+    },
+      {
+    	id: '124',
+    	name: 'Sally',
+    	email: 'sally@gmail.com',
+    	password: 'bananas',
+    	entries: 0,
+    	joined: new Date()
+    }
+]
+}
+
+app.get('/', (req,res) => {
+   res.send('this is working');
+})
+
+app.post('/signin', (req,res)=>{
+  signin.handleSignin(req,res,db,bcrypt)
+})
+
+app.post('/register',(req,res)=>{
+  register.handleRegister(req,res,db,bcrypt);
+})
+
+app.get('/profile/:id' , (req,res)=>{
+  profile.handleProfileGet(req,res,db);
+})
+
+app.put('/image' ,(req,res)=>{
+  image.handleImage(req,res,db);
+})
+
+app.post('/imageurl' ,(req,res)=>{
+  image.handleApiCall(req,res);
+})
+
+app.listen(3000, () => {
+  console.log('app is running');
+})
