a337096b7a699987720e9fc104dec9762c00caac
Umang Raj <umangraj@Umangs-MacBook-Air.local>
2020-11-05 12:41:45 +0530

    database config

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const image=require('./controllers/image');
 const db =knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'umangraj',
-    password : '',
-    database : 'face-app'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
